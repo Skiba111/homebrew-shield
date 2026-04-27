2ab9d9255bd9970ceb086720fdad617538c56b0c
Tim Skiba <skibatima9@gmail.com>
2026-04-27 20:01:07 +0700

    feat: shield-vpn cask v1.0.40 with correct SHA256 and filenames

--- Casks/shield-vpn.rb
+++ Casks/shield-vpn.rb
@@ -2,13 +2,13 @@ cask "shield-vpn" do
   version "1.0.40"
 
   on_arm do
-    url "https://github.com/Skiba111/shield-releases/releases/download/macos-v#{version}/SHIELD VPN-#{version}-arm64.dmg"
-    sha256 :no_check
+    url "https://github.com/Skiba111/shield-releases/releases/download/macos-v#{version}/SHIELD.VPN-#{version}-arm64.dmg"
+    sha256 "150a9d7cb319aa1dcac48da0ece7c07b048dc2a77506a88a2f6259fce4e4467d"
   end
 
   on_intel do
-    url "https://github.com/Skiba111/shield-releases/releases/download/macos-v#{version}/SHIELD VPN-#{version}.dmg"
-    sha256 :no_check
+    url "https://github.com/Skiba111/shield-releases/releases/download/macos-v#{version}/SHIELD.VPN-#{version}.dmg"
+    sha256 "069c793f6bc10840ba2762ed648df7bb7a576362be2bd8d0cd70c8b54d4020c7"
   end
 
   name "SHIELD VPN"
