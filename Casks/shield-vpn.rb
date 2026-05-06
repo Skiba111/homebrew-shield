049d65773336ec3004c90b09eed555e025b2c342
Tim Skiba <skibatima9@gmail.com>
2026-05-06 17:51:33 +0700

    chore: bump cask to v1.0.46

--- Casks/shield-vpn.rb
+++ Casks/shield-vpn.rb
@@ -1,14 +1,14 @@
 cask "shield-vpn" do
-  version "1.0.45"
+  version "1.0.46"
 
   on_arm do
     url "https://github.com/Skiba111/shield-releases/releases/download/macos-v#{version}/SHIELD.VPN-#{version}-arm64.dmg"
-    sha256 "294c450b753a3baa9ce4487c8071cdd615b04399751e1c953823818482e8712b"
+    sha256 "2781739eb4c32481e36f5959f2fcc8bfd98d5c41f6e4033b8e3fee34489c26bf"
   end
 
   on_intel do
     url "https://github.com/Skiba111/shield-releases/releases/download/macos-v#{version}/SHIELD.VPN-#{version}.dmg"
-    sha256 "34d69e80229347ff1e4b4f224804e49a928ef1cd8cf71ddac882d226a0a2d7e3"
+    sha256 "53a9122438fb510885fec356f2432a0e74e98db8b23dff12a3ad9c36f3899dfd"
   end
 
   name "SHIELD VPN"
